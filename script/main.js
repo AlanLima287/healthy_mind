4a02d94f9255f88dd87efe32cde65d9b1f656696
AlanLima287 <alanlimanvc@gmail.com>
2024-05-15 12:00:55 -0300

    New themes, theme support and theme generation tool

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector('menu')
+const select = document.querySelector('select')
 const inputs = [
    document.querySelector('#lgh-0'),
    document.querySelector('#lgh-1'),
@@ -11,14 +11,52 @@ const inputs = [
    document.querySelector('#acc-1')
 ]
 
-for (let i = 0; i < inputs.length; i++) {
-
-   fetch('./assets/themes.json').then(res => res.json()).then(data => {
-      inputs[i].value = data.oldtimes[i]
-   })
-
-   inputs[i].addEventListener('input', evt => {
-      document.documentElement.style.setProperty(`--clr-${evt.target.id}`, evt.target.value)
-   })
+const color_scheme = {
+   selected: false,
+   themes: {}
 }
 
+async function setup() {
+
+   color_scheme.themes = await fetch('./assets/themes.json').then(res => res.json())
+   
+   for (const theme in color_scheme.themes) {
+      const option = document.createElement('option')
+      option.textContent = theme.replaceAll('_', ' ')
+      option.value = theme
+      select.append(option)
+   }
+
+   set_theme('old_times')
+   select.addEventListener('input', evt => { set_theme(evt.target.value) })
+   
+   for (let i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener('input', evt => {
+         document.documentElement.style.setProperty(`--clr-${evt.target.id}`, evt.target.value)
+         color_scheme.selected = false
+      })
+   }
+
+   document.querySelector('#copy').addEventListener('click', copy_palette_to_clipboard)
+}
+
+function set_theme(theme_name) {
+   for (let i = 0; i < inputs.length; i++) {
+      inputs[i].value = color_scheme.themes[theme_name][i]
+      document.documentElement.style.setProperty(`--clr-${inputs[i].id}`, inputs[i].value)
+   }
+
+   color_scheme.selected = true
+}
+
+async function copy_palette_to_clipboard() {
+   let data = '["'
+   for (let i = 0; i < inputs.length - 1; i++) {
+      data += inputs[i].value + '","'
+   }
+   data += inputs[inputs.length - 1].value + '"]'
+
+   await navigator.clipboard.writeText(data)
+}
+
+setup()
